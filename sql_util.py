6131ab0a6fcd0809bb06476ddd9d7bda03888f45
Daniel Tatarkin <danieltatarkin@gmail.com>
2021-01-10 15:34:28 -0600

    Added ability to add tasks with deadline, list today's tasks, week's tasks and all tasks + Refactoring

--- sql_util.py
+++ sql_util.py
@@ -2,20 +2,21 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String, Date
-from datetime import datetime
+from datetime import datetime, timedelta
 
 Base = declarative_base()
 DB_NAME = "todo.db"
 
 
-class Table(Base):
+# Base Task model for SQLite table
+class Task(Base):
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True)
     task = Column(String, default='empty')
     deadline = Column(Date, default=datetime.today())
 
     def __repr__(self):
-        return f"{self.id}. {self.task}"
+        return f"{self.task}"
 
 
 class SqlSession:
@@ -24,10 +25,40 @@ class SqlSession:
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)()
 
-    def add_task(self, task):
-        new_row = Table(task=task)
+    def add_task(self, task, date=datetime.today().date()):
+        """
+        Adds new Task to our SQLite database
+
+        task: Task description string
+        date: Task deadline date
+        """
+        new_row = Task(task=task, deadline=date)
         self.session.add(new_row)
         self.session.commit()
 
-    def get_tasks(self):
-        return self.session.query(Table).all()
+    def get_all_tasks(self):
+        """
+        Retrieves all Tasks from SQLite database
+        """
+        return self.session.query(Task).order_by(Task.deadline).all()
+
+    def get_today_tasks(self):
+        """
+        Retrieves all Tasks where deadline is Today
+        """
+        today = datetime.today()
+        return self.session.query(Task).filter(Task.deadline == today.date()).all()
+
+    def get_week_tasks(self):
+        """
+        Retrieves all Tasks where deadline is within next 7 days
+        """
+        today = datetime.today().date()
+        date_list = [today + timedelta(days=x) for x in range(8)]
+        return self.session.query(Task).filter(Task.deadline.in_(date_list)).order_by(Task.deadline).all()
+
+    def get_date_task(self, date):
+        """
+        Retrieves all Tasks for specific date
+        """
+        return self.session.query(Task).filter(Task.deadline == date).order_by(Task.deadline).all()
--- todolist.py
+++ todolist.py
@@ -1,49 +1,105 @@
+from datetime import datetime, timedelta
 from sql_util import SqlSession
 
-SQL = SqlSession()
-PROMPT = "1) Today's tasks\n" \
-         "2) Add task\n" \
-         "0) Exit"
+
+class ToDoList:
+    PROMPT = "1) Today's tasks\n" \
+             "2) Week's tasks\n" \
+             "3) All tasks\n" \
+             "4) Add task\n" \
+             "0) Exit"
+    TODAY_TASKS = 1
+    WEEK_TASKS = 2
+    ALL_TASKS = 3
+    ADD_TASK = 4
+    EXIT = 0
+
+    def __init__(self):
+        self.SQL = SqlSession()
+
+    def list_tasks(self, choice):
+        today = datetime.today()
+
+        if choice == self.TODAY_TASKS:
+            print(f"\nToday {today.strftime('%d %b')}:")
+            rows_list = self.SQL.get_today_tasks()
+            if len(rows_list) == 0:
+                print("Nothing to do!")
+            else:
+                for i, task in enumerate(rows_list, start=1):
+                    print(i, task, sep='. ')
+            print("")
+
+        elif choice == self.WEEK_TASKS:
+            date_list = [today.date() + timedelta(days=x) for x in range(7)]
+            for date in date_list:
+                print(f'\n{datetime.strftime(date, "%A %d %b:")}')
+                tasks = self.SQL.get_date_task(date)
+                if len(tasks) > 0:
+                    for i, task in enumerate(tasks, start=1):
+                        print(i, task, sep='. ')
+                else:
+                    print("Nothing to do!")
+            print("")
+
+        elif choice == self.ALL_TASKS:
+            print("\nAll tasks:")
+            rows_list = self.SQL.get_all_tasks()
+            if len(rows_list) == 0:
+                print("Nothing to do!")
+            else:
+                for i, task in enumerate(rows_list, start=1):
+                    print(i, task, datetime.strftime(task.deadline, "%d %b"), sep='. ')
+            print("")
+
+    def add_task(self):
+        print("\nEnter task")
+        task = input(">")
+        print("Enter deadline")
+        deadline = input(">")
+        try:
+            if not deadline: deadline = str(datetime.today().date())
+            date = datetime.strptime(deadline, "%Y-%m-%d")
+        except ValueError:
+            print("Error, bad date format, ex: YYYY-mm-dd")
+            self.add_task()
+            return
+        self.SQL.add_task(task, date)
+        print("The task has been added!\n")
+
+    def shutdown(self):
+        print("\nBye!")
+        exit(0)
+
+    def incorrect_input(self):
+        print("Incorrect input, try again\n")
+        self.print_prompt()
+
+    def print_prompt(self):
+        fun_choices = {
+            self.TODAY_TASKS: self.list_tasks,
+            self.WEEK_TASKS: self.list_tasks,
+            self.ALL_TASKS: self.list_tasks,
+            self.ADD_TASK: self.add_task,
+            self.EXIT: self.shutdown
+        }
+        print(self.PROMPT)
+        try:
+            user_choice = int(input("> "))
+        except ValueError:
+            print("Please enter a digit 0-4")
+            self.print_prompt()
+            return
+        if user_choice in range(self.TODAY_TASKS, self.ALL_TASKS + 1):
+            fun_choices.get(user_choice, self.incorrect_input)(user_choice)
+        else:
+            fun_choices.get(user_choice, self.incorrect_input)()
 
 
 def main():
+    app = ToDoList()
     while True:
-        print_prompt()
-
-
-def list_tasks():
-    rows_li = SQL.get_tasks()
-    print("\nToday:")
-    if len(rows_li) == 0:
-        print("Nothing to do!")
-    else:
-        for task in rows_li:
-            print(task)
-    print("")
-
-
-def add_task():
-    print("\nEnter task")
-    task = input(">")
-    SQL.add_task(task)
-    print("The task has been added!\n")
-
-
-def shutdown():
-    print("\nBye!")
-    exit(0)
-
-
-def incorrect_input():
-    print("Incorrect input, try again\n")
-    print_prompt()
-
-
-def print_prompt():
-    print(PROMPT)
-    choice = input("> ")
-    fun_choices = {'1': list_tasks, '2': add_task, '0': shutdown}
-    fun_choices.get(choice, incorrect_input)()
+        app.print_prompt()
 
 
 if __name__ == '__main__':
